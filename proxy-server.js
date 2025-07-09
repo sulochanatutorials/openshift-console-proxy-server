8b4e580d9a4fa41306b959c80f1155ac4ea2b434
Ganesh kanade <ganesh.kanade@stackconsole.io>
2025-07-09 13:06:35 +0530

    Encryption adedd

--- proxy-server.js
+++ proxy-server.js
@@ -1,6 +1,44 @@
 require("dotenv").config(); // Load .env
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+// Add this 👇
+const { webcrypto } = require("crypto");
+const crypto = webcrypto;
+
+function base64ToArrayBuffer(base64) {
+  const binary = atob(base64);
+  const len = binary.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) bytes[i] = binary.charCodeAt(i);
+  return bytes.buffer;
+}
+async function decryptApiToken(encrypted, base64Key) {
+  const [ivB64, cipherB64, tagB64] = encrypted.split(":");
+  const iv = base64ToArrayBuffer(ivB64);
+  const cipherText = base64ToArrayBuffer(cipherB64);
+  const tag = base64ToArrayBuffer(tagB64);
+
+  const keyRaw = base64ToArrayBuffer(base64Key);
+  const cryptoKey = await crypto.subtle.importKey(
+    "raw",
+    keyRaw,
+    { name: "AES-GCM" },
+    false,
+    ["decrypt"]
+  );
+
+  const combined = new Uint8Array(cipherText.byteLength + tag.byteLength);
+  combined.set(new Uint8Array(cipherText), 0);
+  combined.set(new Uint8Array(tag), cipherText.byteLength);
+
+  const decrypted = await crypto.subtle.decrypt(
+    { name: "AES-GCM", iv: iv },
+    cryptoKey,
+    combined
+  );
+
+  return new TextDecoder().decode(decrypted);
+}
 
 const WebSocket = require("ws");
 const axios = require("axios");
@@ -60,16 +98,17 @@ wss.on("connection", async (clientSocket, req) => {
 
     const { namespace, vm, api_token } = response.data;
 
-    console.log(
-      `[INFO] Token validated for VM: ${vm} in namespace: ${namespace}`
-    );
+    const sharedKey = process.env.VNC_API_ENCRYPTION_KEY.replaceAll("", "");
+    const apiToken = await decryptApiToken(api_token, sharedKey);
+
+    console.log(`[INFO] decrypted token:${apiToken}`);
 
     const openshiftUrl = `wss://${OPENSHIFT_VM_CONSOLE_WS_ENDPOINT}/apis/subresources.kubevirt.io/v1/namespaces/${namespace}/virtualmachineinstances/${vm}/vnc`;
     console.log(`[DEBUG] Connecting to OpenShift WebSocket: ${openshiftUrl}`);
 
     const targetSocket = new WebSocket(openshiftUrl, {
       headers: {
-        Authorization: `Bearer ${api_token}`,
+        Authorization: `Bearer ${apiToken}`,
       },
     });
 
